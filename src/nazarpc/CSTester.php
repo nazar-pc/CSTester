7dfa05e07131fc477027fa08a6196d3f0534203e
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-29 23:21:48 +0200

    composer update

--- src/nazarpc/CSTester.php
+++ src/nazarpc/CSTester.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		CSTester
- * @version		0.1.7
+ * @version		0.1.8
  * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @copyright	Copyright (c) 2013, Nazar Mokrynskyi
  * @license		MIT License, see license.txt
